4226213169ea3da55bc4bee90b7b1cfb70769de5
Aduvai1 <adnansarkar111@gmail.com>
2021-06-04 01:08:27 +0600

    Login Page Intrigation By Aduvai1

--- App.js
+++ App.js
@@ -1,28 +1,31 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { createContext, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import Home from "./components/Home/Home";
 import Details from "./components/Details/Details";
+import { LoginScreen } from "./components/Login/LoginScreen";
+
+export const loginContext = createContext();
 
 const Drawer = createDrawerNavigator();
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Drawer.Navigator>
-        <Drawer.Screen name="Home" component={Home} />
-        <Drawer.Screen name="Details" component={Details} />
-      </Drawer.Navigator>
-    </NavigationContainer>
-  );
+    const [loginInfo, setLoginInfo] = useState();
+    console.log(loginInfo);
+    return (
+        <loginContext.Provider value={{ setLoginInfo }}>
+            <NavigationContainer>
+                <Drawer.Navigator>
+                    <Drawer.Screen name={loginInfo ? "logOut" : "LogIn"} component={LoginScreen} />
+                    {loginInfo && (
+                        <>
+                            <Drawer.Screen name="Home" component={Home} />
+                            <Drawer.Screen name="Details" component={Details} />
+                        </>
+                    )}
+                </Drawer.Navigator>
+            </NavigationContainer>
+        </loginContext.Provider>
+    );
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#fff",
-//     alignItems: "center",
-//     justifyContent: "center",
-//   },
-// });
